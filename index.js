ae5e4332a5402a34b1e3973ebb31d28c7d53dd50
garden3r <168115492+garden3r@users.noreply.github.com>
2024-04-26 18:56:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,39 @@ function greeting() {
   window.alert(`Hello ${usr}`);
 
 }
-greeting();
+greeting()
+   document.addEventListener('DOMContentLoaded', (event) => {
+        const savedName = localStorage.getItem('userName');
+        if(savedName){
+            document.getElementById('nameForm').style.display = 'none';
+            document.getElementById('loginForm').style.display = 'block';
+        }
+    });
+    document.getElementById('nameForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const password = document.getElementById('password').value;
+        if (!name || !password) {
+            window.alert("enter a password and a user please");
+            return;
+        }
+        localStorage.setItem('userName', name);
+        localStorage.setItem('password', password);
+        location.reload();
+    });
+
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const password = document.getElementById('loginPassword').value;
+        const savedPassword = localStorage.getItem('password');
+        if (password !== savedPassword) {
+            window.alert("password does not exist");
+            return;
+        }
+        document.getElementById('loginForm').style.display = 'none';
+        const savedName = localStorage.getItem('userName');
+        document.getElementById('welcomeMessage').textContent = 'Hello, ' + savedName; 
+        document.getElementById('userName').innerText = savedName;
+    });
